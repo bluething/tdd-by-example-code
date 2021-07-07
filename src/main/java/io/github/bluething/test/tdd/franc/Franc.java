73967298a99d907d84aaf8e14cbf2052a780473c
Habib Ali Machpud <habib.gunadarma@gmail.com>
2021-07-07 09:53:04 +0700

    Make the test pass
    No abstraction, no clean design. Juts to make the test pass

--- src/main/java/io/github/bluething/test/tdd/franc/Franc.java
+++ src/main/java/io/github/bluething/test/tdd/franc/Franc.java
@@ -1,10 +1,18 @@
 package io.github.bluething.test.tdd.franc;
 
 public class Franc {
+    private final long amount;
     public Franc(long amount) {
+        this.amount = amount;
     }
 
     public Franc multiplyBy(int i) {
-        return null;
+        return new Franc(amount * i);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        long amount = ((Franc)obj).amount;
+        return this.amount == amount;
     }
 }
